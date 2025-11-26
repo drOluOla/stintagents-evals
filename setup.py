cabf79602f00ecfb83dc84b16be61097074fdcf1
Oluseyi <oluseyiolarewaju@outlook.com>
2025-11-26 13:54:20 +0000

    Updating version for compartibility

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     python_requires=">=3.8",
     install_requires=[
         "openai>=1.59.7",
-        "openai-agents[voice]==0.5.0",
+        "openai-agents[voice]==0.4.2",
         "gradio==5.49.1",
         "numpy>=2.1.3",
         "torch>=2.5.1",
